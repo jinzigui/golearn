c19f225bfbde8667543266f16f2705aad0c87e32
李将 <a.xsk@lijiangdeMacBook-Pro.local>
2017-06-02 22:01:10 +0800

    array

--- array.go
+++ array.go
@@ -9,4 +9,20 @@ func main(){
 	for i:= 0; i< len(array);i++{
 		fmt.Printf("array at index %d is %d\n", i, array[i])
 	}
+	for i,_:=range array{	
+		fmt.Printf("array at index %d is %d\n", i, array[i])
+	}
+	var strarray = [6]string{2:"test", 4:"jj"}
+	for i,_:=range strarray{	
+		fmt.Printf("strarray at index %d is %s\n", i,strarray[i])
+	}
+	
+	fmt.Printf("sum = %d\n", sum(&array))
+}
+
+func sum(array *[5] int)(sum int){
+	for _, v:= range array{
+		sum += v
+	}
+	return
 }
