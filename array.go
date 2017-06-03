663de7c8ace740f7eb5d09233af36ba73a070aa8
李将 <a.xsk@lijiangdeMacBook-Pro.local>
2017-06-03 11:45:55 +0800

    slice and array

--- array.go
+++ array.go
@@ -18,6 +18,8 @@ func main(){
 	}
 	
 	fmt.Printf("sum = %d\n", sum(&array))
+	slices()
+	makeslice()
 }
 
 func sum(array *[5] int)(sum int){
@@ -25,4 +27,32 @@ func sum(array *[5] int)(sum int){
 		sum += v
 	}
 	return
+}
+
+func slices(){
+	var array = [6] int {1,4,2,6,7,3}
+	var slice []int = array[1:4]
+	//var slice2 = []int{2,4,7}
+	for i,_ := range slice{
+		fmt.Printf("slice index = %d, value = %d\n",i, slice[i])
+	} 
+}
+
+func makeslice(){
+	var array = [6] int {1,4,2,6,7,3}
+	var slice []int = array[1:4]
+	printslice(slice)
+	var slice1 []int = make([]int, 10)
+	printslice(slice1)
+	var slice2 []int = make([]int, 5,10)
+	printslice(slice2)
+}
+
+func printslice(slice []int){	
+	for i:= 0; i < len(slice);i++{
+		slice[i] = 2*i;
+	}
+	for i:= 0; i < len(slice);i++{
+		fmt.Printf("slice[%d]= %d\n", i, slice[i])
+	}
 }
