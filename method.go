6c0b0557a12a04f4ffe6661c11830531f9b60077
李将 <a.xsk@lijiangdeMacBook-Pro.local>
2017-06-04 21:34:18 +0800

    method like c++ class

--- method.go
+++ method.go
@@ -18,8 +18,26 @@ func (structPeople *structTest) PrintfStruct() {
 	fmt.Printf("structTest.nAge = %d, structTest.strName = %s\n", structPeople.nAge, structPeople.strName)
 }
 
+type intVector []int
+
+func (vector *intVector) Sum() (sum int) {
+	for _, num := range *vector {
+		sum += num
+	}
+	return
+}
+
+func (vector intVector) Reduction() (result int) {
+	for _, num := range vector {
+		result -= num
+	}
+	return
+}
+
 func main() {
 	structPeople := new(structTest)
 	structPeople.InitStruct(20, "test")
 	structPeople.PrintfStruct()
+	fmt.Println((&intVector{2, 4, 7}).Sum())
+	fmt.Println(intVector{3, 5, 8}.Reduction())
 }
