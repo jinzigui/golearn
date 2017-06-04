9d73838c30f81067128d0a96f85171d3a18aca2c
李将 <a.xsk@lijiangdeMacBook-Pro.local>
2017-06-04 22:40:05 +0800

    method like c++ class

--- method.go
+++ method.go
@@ -4,18 +4,35 @@ import (
 	"fmt"
 )
 
-type structTest struct {
+type structPeople struct {
 	nAge    int
 	strName string
 }
 
-func (structPeople *structTest) InitStruct(nage int, strname string) {
+func (structPeople *structPeople) InitStructPeople(nage int, strname string) {
 	structPeople.nAge = nage
 	structPeople.strName = strname
 }
 
-func (structPeople *structTest) PrintfStruct() {
-	fmt.Printf("structTest.nAge = %d, structTest.strName = %s\n", structPeople.nAge, structPeople.strName)
+func (structPeople *structPeople) PrintfStructPeople() {
+	fmt.Printf("structPeople.nAge = %d, structPeople.strName = %s\n", structPeople.nAge, structPeople.strName)
+}
+
+type structStudent struct {
+	structPeople
+	nNumID int
+	fScore float32
+}
+
+func (structStudent *structStudent) InitStructStudent(nage int, strname string, nnumid int, fscore float32) {
+	structStudent.InitStructPeople(nage, strname)
+	structStudent.nNumID = nnumid
+	structStudent.fScore = fscore
+}
+
+func (structStudent *structStudent) PrintfStructStudent() {
+	structStudent.PrintfStructPeople()
+	fmt.Printf("structStudent.nNumID = %d, structStudent.fScore = %f\n", structStudent.nNumID, structStudent.fScore)
 }
 
 type intVector []int
@@ -35,9 +52,18 @@ func (vector intVector) Reduction() (result int) {
 }
 
 func main() {
-	structPeople := new(structTest)
-	structPeople.InitStruct(20, "test")
-	structPeople.PrintfStruct()
+	structPeople1 := new(structPeople)
+	structPeople1.InitStructPeople(20, "test")
+	structPeople1.PrintfStructPeople()
+
 	fmt.Println((&intVector{2, 4, 7}).Sum())
 	fmt.Println(intVector{3, 5, 8}.Reduction())
+
+	structStudent1 := &structStudent{structPeople{22, "structStudent"}, 1200350116, 99.5}
+	structStudent1.PrintfStructStudent()
+	structStudent1.PrintfStructPeople()
+
+	structStudent2 := new(structStudent)
+	structStudent2.InitStructStudent(23, "酱酱", 1200350116, 99.5)
+	structStudent2.PrintfStructStudent()
 }
