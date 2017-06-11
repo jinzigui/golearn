b7dce0280cc8c170ebb9a533b7544e2db0f5fccd
李将 <a.xsk@lijiangdeMacBook-Pro.local>
2017-06-11 16:02:36 +0800

    update interface

--- interface.go
+++ interface.go
@@ -26,6 +26,23 @@ func (camera *Camera) LeftElectricity() int {
 	return camera.nElectricity
 }
 
+func emptyInterFace() {
+	testFunc := func(any interface{}) {
+		switch v := any.(type) {
+		case int:
+			fmt.Printf("this is an num = %v\n", v)
+		case string:
+			fmt.Printf("this is a string =%v\n", v)
+		default:
+			fmt.Printf("this is not deal with type ,value = %v", v)
+		}
+	}
+	var strmsg string = "this is empty interface test"
+	testFunc(strmsg)
+	testFunc(233)
+	testFunc(3.14)
+}
+
 func main() {
 	phone := &Phone{68, "IPhone"}
 	camera := &Camera{49, "Canon"}
@@ -47,4 +64,6 @@ func main() {
 			fmt.Println("not macth")
 		}
 	}
+
+	emptyInterFace()
 }
