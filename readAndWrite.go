f88ffc449e66d9b388ff6a30b43ce45c1ea68f65
李将 <a.xsk@lijiangdeMacBook-Pro.local>
2017-06-17 11:05:44 +0800

    read and write

---
+++ readAndWrite.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+type peopleInfo struct {
+	nAge    int
+	fHegith float32
+	fWegith float32
+	strName string
+}
+
+func (people *peopleInfo) printPeopleInfo() {
+	fmt.Printf("your name is %s, ages = %d, hegith = %f, wegith = %f\n",
+		people.strName, people.nAge, people.fHegith, people.fWegith)
+}
+
+func main() {
+	people := new(peopleInfo)
+	fmt.Println("input peopel some base info")
+	fmt.Println("now input your name:")
+	fmt.Scanf("%s", &people.strName)
+	fmt.Println("input your ages:")
+	fmt.Scanln(&people.nAge)
+	fmt.Println("input your hegith:")
+	fmt.Scanln(&people.fHegith)
+	fmt.Println("input your wegith:")
+	fmt.Scanln(&people.fWegith)
+	people.printPeopleInfo()
+}
