1ac6d7f4d9f6a3529186b82f4198eea91e9766c5
李将 <a.xsk@lijiangdeMacBook-Pro.local>
2017-06-17 13:54:15 +0800

    read and write func

--- readAndWrite.go
+++ readAndWrite.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 )
 
@@ -18,17 +20,7 @@ func (people *peopleInfo) printPeopleInfo() {
 		people.strName, people.nAge, people.fHegith, people.fWegith)
 }
 
-func bufferReadTest() string {
-	inputReader := bufio.NewReader(os.Stdin)
-	fmt.Println("input people some base info")
-	msg, err := inputReader.ReadString('\n')
-	if err != nil {
-
-	}
-	return msg
-}
-
-func main() {
+func scanfTest() {
 	people := new(peopleInfo)
 	fmt.Println("input peopel some base info")
 	fmt.Println("now input your name:")
@@ -40,6 +32,62 @@ func main() {
 	fmt.Println("input your wegith:")
 	fmt.Scanln(&people.fWegith)
 	people.printPeopleInfo()
+}
 
+func bufferReadTest() string {
+	inputReader := bufio.NewReader(os.Stdin)
+	fmt.Println("input people some base info")
+	msg, err := inputReader.ReadString('\n')
+	if err != nil {
+
+	}
+	return msg
+}
+
+func readFileTest() {
+	file, openFileerr := os.Open("test.txt")
+	if openFileerr != nil {
+		fmt.Println("open file failure")
+		return
+	}
+
+	fileReader := bufio.NewReader(file)
+	for {
+		strText, readerr := fileReader.ReadString('\n')
+		fmt.Println(strText)
+		if readerr == io.EOF {
+			break
+		}
+	}
+	file.Close()
+	//os open file, bufio.NewReader control file reader, reader read text
+
+	//another read way
+	buf, err := ioutil.ReadFile("test.txt")
+	if err != nil {
+		fmt.Println("open file failure")
+	}
+	fmt.Printf("%s", string(buf))
+}
+
+func writeFileTest() {
+	file, fileerr := os.OpenFile("test.txt", os.O_WRONLY|os.O_CREATE, 0666)
+	if fileerr != nil {
+		fmt.Println("open file failure")
+		return
+	}
+	writeReader := bufio.NewWriter(file)
+	strMsg := "just write test\n"
+	writeReader.WriteString(strMsg)
+	writeReader.Flush()
+
+	file.WriteString("file control writestring fun\n") //call writeString func
+	file.Close()
+}
+
+func main() {
+	scanfTest()
 	fmt.Println(bufferReadTest())
+	writeFileTest()
+	readFileTest()
 }
